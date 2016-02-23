748f6958d564a3df5dbd363f8587735af876e04a
yeinermeres <yeinerrestrepo@hotmail.com>
2016-02-23 08:45:15 -0500

    Primer commit

---
+++ WebApplication1/Dal/ModelContex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace Dal
+{
+    public class ModelContex : DbContext
+    {
+        public DbSet<Configuraciones> Configuracion { get; set; }
+        public DbSet<Proyec_Manager> Pro_Manager { get; set; }
+        public DbSet<Proyecto> Proyecto { get; set; }
+        public DbSet<Proceso_Competitivo> Proceso_Competitivo { get; set; }
+        public DbSet<Archivo_Ruta> Archivo_Ruta { get; set; }
+
+         
+    }
+}
---
+++ WebApplication1/Dal/Proceso_Competitivo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Dal
+{
+    public class Proceso_Competitivo
+    {
+        [Key]
+        public int ID_COMPETITIVO { get; set; }
+        public string PROCESO { get; set; }
+        public string PROCESO_INICIO { get; set; }
+        public int TIEMPO_PROCESO { get; set; }
+        public string FECHA_INICO { get; set; }
+        public string FECHA_INIC_SERVICE { get; set; }
+        public int TIEMPO_EJECUCION { get; set; }
+        public string DETALLE_PS { get; set; }
+        public int CANTIDAD { get; set; }
+        public string UNIDAD { get; set; }
+        public string LUGAR_EJECUCION { get; set; }
+        public decimal VALOR_ESTIMADO { get; set; }
+        public decimal VALOR_TOTAL { get; set; }
+        /// <summary>
+        /// relacion proceso competitivos => proyectos
+        /// </summary>
+        public int PROYECTO_COMPETITIVO { get; set; }
+
+        [ForeignKey("PROCESO_ARCHIVO")]
+         public virtual ICollection<Archivo_Ruta> Ruta_Achivo { get; set; }
+    }
+}
